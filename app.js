4c12af8fc4fa9dc650d6c91820fb7c72bcddc45b
Sourab Hossain <cpsourab@gmail.com>
2020-05-16 01:45:50 +0600

    app

---
+++ app.js
@@ -0,0 +1,14 @@
+// Budget Controller
+var budgetController = (function() {
+
+})();
+
+// UI Controller
+var UIController = (function() {
+
+})();
+
+// Global App Controller
+var controller = (function(budgetCtrl, UICtrl) {
+
+})(budgetController, UIController);
