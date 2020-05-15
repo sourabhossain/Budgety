16550b979c838f62ca341b6f94b8d2be3bd7f668
Sourab Hossain <cpsourab@gmail.com>
2020-05-16 02:11:25 +0600

    init ui

--- app.js
+++ app.js
@@ -11,4 +11,20 @@ var UIController = (function() {
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
 
+	document.querySelector(".add__btn").addEventListener("click", function() {
+		// 1. Get the filed input data
+
+		// 2. Add the item to the budget controller
+
+		// 3. Add the item to the UI
+
+		// 4. Calculate the budget
+
+		// 5. Display the budget on the UI
+	});
+
+	document.addEventListener("keypress", function() {
+		
+	});
+
 })(budgetController, UIController);
