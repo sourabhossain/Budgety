d456ea0a22aad1b3e981e3fa1bebe432ba3f5302
Sourab Hossain <cpsourab@gmail.com>
2020-05-16 02:17:00 +0600

    ctrlAddItem

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ var UIController = (function() {
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
-
-	document.querySelector(".add__btn").addEventListener("click", function() {
-		// 1. Get the filed input data
+    
+    var ctrlAddItem = function() {
+    	// 1. Get the filed input data
 
 		// 2. Add the item to the budget controller
 
@@ -21,10 +21,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 4. Calculate the budget
 
 		// 5. Display the budget on the UI
-	});
+    }
 
-	document.addEventListener("keypress", function() {
-		
+	document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+	document.addEventListener("keypress", function(event) {
+	    
+	    if(event.keyCode === 13 || event.which === 13) {
+	    	ctrlAddItem();
+	    }
+
 	});
 
 })(budgetController, UIController);
