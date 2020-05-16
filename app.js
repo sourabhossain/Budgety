e10b0b761e9578d32c781a9aa0f30979cfb7433d
Sourab Hossain <cpsourab@gmail.com>
2020-05-17 02:08:17 +0600

    modified

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 // Budget Controller
 var budgetController = (function() {
 
+    // 
+
 })();
 
 // UI Controller
 var UIController = (function() {
+    var DOMstrings = {
+    	inputType: ".add__type",
+    	inputDescription: ".add__description",
+    	inputValue: ".add__value",
+    	inputBtn: ".add__btn"
+    };
+
+    return {
+    	getinput: function() {
+    		return {
+	    		type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+	    		description: document.querySelector(DOMstrings.inputDescription).value,
+	            value: document.querySelector(DOMstrings.inputValue).value 
+            }
+    	},
+
+    	getDOMstrings: function() {
+    		return DOMstrings;
+    	}
+    };
 
 })();
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
     
+    var setupEventListeners = function() {
+    	
+    };
+
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
     	// 1. Get the filed input data
+    	var input = UICtrl.getinput();
 
 		// 2. Add the item to the budget controller
 
@@ -23,7 +52,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 5. Display the budget on the UI
     }
 
-	document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
 	document.addEventListener("keypress", function(event) {
 	    
