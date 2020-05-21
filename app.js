0785da211afbc653da73d3e9704fdba8b0010c86
Sourab Hossain <cpsourab@gmail.com>
2020-05-21 21:41:26 +0600

    BUDGET CONTROLLER

--- app.js
+++ app.js
@@ -1,135 +1,148 @@
-// Budget Controller
+// BUDGET CONTROLLER
 var budgetController = (function() {
-
-    var Expense = funciton(id, description, value) {
-    	this.id = id;
-    	this.description = description;
-    	this.value = value;
-    	this.percentage = -1;
+    
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.percentage = -1;
     };
-
+    
+    
     Expense.prototype.calcPercentage = function(totalIncome) {
-    	if(totalIncome > 0) {
-    		this.percentage = Math.round((this.value / totalIncome) * 100);
-    	} else {
-    		this.percentage = -1;
-    	}
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
     };
-
+    
+    
     Expense.prototype.getPercentage = function() {
-    	return this.percentage;
+        return this.percentage;
     };
-
+    
+    
     var Income = function(id, description, value) {
-    	this.id = id;
-    	this.description = description;
-    	this.value = value;
+        this.id = id;
+        this.description = description;
+        this.value = value;
     };
-
+    
+    
     var calculateTotal = function(type) {
-    	var sum = 0;
-
-    	data.allItem[type].forEach(function(cur) {
-    		sum += cur.value;
-    	});
-
-    	data.totals[type] = sum;
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
     };
-
+    
+    
     var data = {
-    	allItems: {
-    		exp: [],
-    		inc: []
-    	},
-    	totals: {
-    		exp: 0,
-    		inc: 0
-    	}
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        },
+        budget: 0,
+        percentage: -1
     };
-
+    
+    
     return {
-    	addItem: function(type, des, val) {
-    		var newItem, ID;
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+            
+            // Return the new element
+            return newItem;
+        },
+        
+        
+        deleteItem: function(type, id) {
+            var ids, index;
+            
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
 
-    		// create new ID
-    		if(data.allItems[type].length > 0) {
-    			ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-    		} else {
-    			ID = 0;
-    		}
+            index = ids.indexOf(id);
 
-    		// Create new item based on "inc" or "exp" type
-    		if(type === "exp") {
-    			newItem = new Expense(ID, des, val);
-    		} else {
-    			newItem = new Income(ID, des, val);
-    		}
-
-    		// push it into our data structure 
-    		data.allItem[type].push(newItem);
-
-    		// return the new element 
-    		return newItem;
-    	},
-
-    	deleteItem: function(type, id) {
-    		var ids, index;
-
-    		ids = data.allItems[type].map(function(current) {
-    			return current.id;
-    		});
-
-    		index = ids.indexOf(id);
-
-    		if(index !== -1) {
-    			data.allItems[type].splice(index, 1);
-    		}
-    	},
-
-    	calculateBudget: function() {
-    		// calculate total income and expenses
-    		calculateTotal("exp");
-    		calculateTotal("inc");
-
-    		// calculate the budget: income - expenses
-    		data.budget = data.totals.inc - data.totals.exp;
-
-    		// calculate the percentage of income that we spent
-    		if(data.totals.inc > 0) {
-    			data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
-    		} else {
-    			data.percentage = -1;
-    		}
-    	}, 
-
-    	calculatePercentages: function() {
-    		data.allItems.exp.forEach(function(cur) {
-    			cur.calcPercentage(data.totals.inc);
-    		});
-    	},
-
-    	getPercentage: function() {
-    		var allPerc = data.allItems.exp.map(function(cur) {
-    			return cur.getPercentage();
-    		});
-
-    		return allPerc;
-    	},
-
-    	getBudget: function() {
-    		return {
-    			budget: data.budget,
-    			totalInc: data.totals.inc,
-    			totalExp: data.totals.exp,
-    			percentage: data.percentage
-    		};
-    	},
-
-    	testing: function() {
-    		console.log(data);
-    	} 
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+            
+        },
+        
+        
+        calculateBudget: function() {
+            
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            
+            // calculate the percentage of income that we spent
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }            
+            
+            // Expense = 100 and income 300, spent 33.333% = 100/300 = 0.3333 * 100
+        },
+        
+        calculatePercentages: function() {
+            data.allItems.exp.forEach(function(cur) {
+               cur.calcPercentage(data.totals.inc);
+            });
+        },
+        
+        
+        getPercentages: function() {
+            var allPerc = data.allItems.exp.map(function(cur) {
+                return cur.getPercentage();
+            });
+            return allPerc;
+        },
+        
+        
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
+        },
+        
+        testing: function() {
+            console.log(data);
+        }
     };
-
+    
 })();
 
 // UI Controller
@@ -138,8 +151,17 @@ var UIController = (function() {
     var DOMstrings = {
     	inputType: ".add__type",
     	inputDescription: ".add__description",
-    	inputValue: ".add__value",
-    	inputBtn: ".add__btn"
+    	inputValue: ".add__value", 
+    	inputBtn: ".add__btn",
+    	incomeContainer: ".income__list",
+        expensesContainer: ".expenses__list",
+        budgetLabel: ".budget__value",
+        incomeLabel: ".budget__income--value",
+        expensesLabel: ".budget__expenses--value",
+        percentageLabel: ".budget__expenses--percentage",
+        container: ".container",
+        expensesPercLabel: ".item__percentage",
+        dateLabel: ".budget__title--month"
     };
 
     return {
